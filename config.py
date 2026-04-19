d9dd2cb8d0b86e459e2ce2eaf96ba2a8a531fc10
whosool622-creator <whosool622@gmail.com>
2026-04-20 01:54:47 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+class Settings:
+    # Токен сообщества ВКонтакте
+    VK_TOKEN = "ВАШ_ТОКЕН_ГРУППЫ"
+    
+    # Префикс команд
+    COMMAND_PREFIX = "/"
+    
+    # Настройки MySQL
+    MYSQL_HOST = "localhost"
+    MYSQL_PORT = 3306
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "ваш_пароль"
+    MYSQL_DATABASE = "vk_bot"
+
+settings = Settings()
