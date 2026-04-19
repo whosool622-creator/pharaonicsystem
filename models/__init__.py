8130eca60083865896e115aea3a71f8392738866
whosool622-creator <whosool622@gmail.com>
2026-04-20 02:00:17 +0300

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from models.role import RoleManager, RoleLevel, ROLE_NAMES, ROLE_PERMISSIONS
+from models.history import HistoryManager, ActionType
+
+__all__ = [
+    "RoleManager", "RoleLevel", "ROLE_NAMES", "ROLE_PERMISSIONS",
+    "HistoryManager", "ActionType"
+]
